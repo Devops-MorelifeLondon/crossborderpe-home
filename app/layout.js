c2c1cf4df65201012d2bad016ca512c33328f1d2
Devops-MorelifeLondon <devops@morelifelondon.net>
2025-11-13 18:15:29 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import NextTopLoader from "nextjs-toploader";
-import ReCaptchaProviderWrapper from "@/components/ReCaptchaProviderWrapper"; // ✅ import wrapper
+import ReCaptchaProviderWrapper from "@/components/ReCaptchaProviderWrapper";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
- title: "CrossborderPe – Cross-Border Payments & Global Finance Solutions",
+  title: "CrossborderPe – Cross-Border Payments & Global Finance Solutions",
   description:
     "Manage international payments, multi-currency accounts, and global business operations with secure, transparent, and fully compliant cross-border solutions.",
 };
@@ -25,6 +26,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* ✅ Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "u5g8py0ewt");
+          `}
+        </Script>
+
         <ReCaptchaProviderWrapper>
           <NextTopLoader height={3} color="blue" />
           <NavBar />
